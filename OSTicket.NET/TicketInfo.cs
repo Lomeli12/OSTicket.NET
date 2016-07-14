2c49f51c179204fe0b799bc940c94400c7139e50
Lomeli12 <anthonylomeli11@gmail.com>
2016-07-14 09:48:02 -0700

    Initial commit.

---
+++ OSTicket.NET/TicketInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OSTicket.NET {
+	public class TicketInfo {
+		private string _name, _email, _phone, _subject, _message;
+		public TicketInfo(string name, string email, string phone, string subject, string message) {
+			this._name = name;
+			this._email = email;
+			this._phone = phone;
+			this._subject = subject;
+			this._message = message;
+		}
+
+		public string name {
+			get { 
+				return _name;
+			}
+		}
+
+		public string email {
+			get {
+				return this._email;
+			}
+		}
+
+		public string phone {
+			get {
+				return this._phone;
+			}
+		}
+
+		public string subject {
+			get {
+				return this._subject;
+			}
+		}
+
+		public string message {
+			get {
+				return this._message;
+			}
+		}
+	}
+}
+
---
+++ OSTicket.NET_Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OSTicket.NET;
+
+namespace OSTicket.NET_Test {
+	class MainClass {
+		public static void Main(string[] args) {
+			if (args != null && args.Length >= 2) {
+				var session = new OSTicketSession(args[0], args[1]);
+				var newTicket = new TicketInfo("TestBot", "bot@lomeli12.net", "0123456789", "Testing Library", "Library Test");
+				var response = session.submitTicket(newTicket, false);
+				Console.WriteLine(response);
+			}
+		}
+	}
+}
