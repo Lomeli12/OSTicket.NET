d2314a70a550695ffa065f8a48f83a1fd8cb8089
Lomeli12 <anthonylomeli11@gmail.com>
2016-07-14 09:56:58 -0700

    Converted all tabs into spaces

--- OSTicket.NET/OSTicketSession.cs
+++ OSTicket.NET/OSTicketSession.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 
 namespace OSTicket.NET {
-	public class OSTicketSession {
-		private string _url, _apikey;
-		public OSTicketSession (string url, string apikey) {
-			this._apikey = apikey;
-			this._url = url;
-		}
+    public class OSTicketSession {
+        private string _url, _apikey;
+        public OSTicketSession (string url, string apikey) {
+            this._apikey = apikey;
+            this._url = url;
+        }
 
-		public string submitTicket(TicketInfo ticket, bool useHTTP) {
-			if (ticket == null) return;
-			return PostHelper.postOSTicketJson(URL, ticket, API_KEY, useHTTP);
-		}
+        public string submitTicket(TicketInfo ticket, bool useHTTP) {
+            if (ticket == null) return;
+            return PostHelper.postOSTicketJson(URL, ticket, API_KEY, useHTTP);
+        }
 
-		public string URL {
-			get { 
-				return _url; 
-			}
-		}
+        public string URL {
+            get { 
+                return _url; 
+            }
+        }
 
-		public string API_KEY {
-			get { 
-				return _apikey; 
-			}
-		}
-
-	}
+        public string API_KEY {
+            get { 
+                return _apikey; 
+            }
+        }
+    }
 }
 
--- OSTicket.NET/PostHelper.cs
+++ OSTicket.NET/PostHelper.cs
@@ -5,34 +5,34 @@ using System.Text;
 using Newtonsoft.Json;
 
 namespace OSTicket.NET {
-	public class PostHelper {
-		private PostHelper(){
-		}
+    public class PostHelper {
+        private PostHelper(){
+        }
 
-		public static string postOSTicketJson(string url, TicketInfo ticket, string apiKey, bool useHTTP) {
-			string value;
-			string apiURL = url + (useHTTP ? "/api/http.php/tickets.json" : "/api/tickets.json");
-			var request = (HttpWebRequest)WebRequest.Create(apiURL);
-			request.Method = "POST";
-			request.ContentType = "application/json";
-			request.Headers.Add("X-API-KEY", apiKey);
-			using (var stream = new StreamWriter(request.GetRequestStream())) {
-				stream.Write(JsonConvert.SerializeObject(ticket));
-				stream.Flush();
-				stream.Close();
-			}
-			Console.WriteLine(apiURL);
-			var response = (HttpWebResponse)request.GetResponse();
-			value = new StreamReader(response.GetResponseStream()).ReadToEnd();
-			return value;
-		}
+        public static string postOSTicketJson(string url, TicketInfo ticket, string apiKey, bool useHTTP) {
+            string value;
+            string apiURL = url + (useHTTP ? "/api/http.php/tickets.json" : "/api/tickets.json");
+            var request = (HttpWebRequest)WebRequest.Create(apiURL);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers.Add("X-API-KEY", apiKey);
+            using (var stream = new StreamWriter(request.GetRequestStream())) {
+                stream.Write(JsonConvert.SerializeObject(ticket));
+                stream.Flush();
+                stream.Close();
+            }
+            Console.WriteLine(apiURL);
+            var response = (HttpWebResponse)request.GetResponse();
+            value = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return value;
+        }
 
-		public static string getPublicIP() {
-			var ip = new WebClient().DownloadString("http://icanhazip.com/");
-			if (ip.EndsWith("\n"))
-				ip = ip.Substring(0, ip.Length - 1);
-			return ip;
-		}
-	}
+        public static string getPublicIP() {
+            var ip = new WebClient().DownloadString("http://icanhazip.com/");
+            if (ip.EndsWith("\n"))
+                ip = ip.Substring(0, ip.Length - 1);
+            return ip;
+        }
+    }
 }
 
