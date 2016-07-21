c54880feed4ca7c585ed177a79461d258206ab0c
Lomeli12 <anthonylomeli11@gmail.com>
2016-07-21 11:27:18 -0700

    Removed another pointless debug line.

--- OSTicket.NET_Test/Program.cs
+++ OSTicket.NET_Test/Program.cs
@@ -5,7 +5,6 @@ namespace OSTicket.NET_Test {
     class MainClass {
         public static void Main(string[] args) {
             if (args != null && args.Length >= 2) {
-                Console.WriteLine(args[0] + " | " + args[1]);
                 var session = new OSTicketSession(args[0], args[1]);
                 var ticket = new Ticket("Dummy User", "dummy@mywebsite.net", "1234567890", "Test Ticket", "This is a test ticket, plz ignore");
                 var id = session.submitTicket(ticket);
